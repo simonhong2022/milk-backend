71ac3e3ad09ba1c15a1a4691e5eb3c8f5b1b9c2a
simon <simonhong85@gmail.com>
2023-05-26 13:07:10 +0200

    create project

---
+++ src/main/java/pgp/week4/milk/MilkApplication.java
@@ -0,0 +1,13 @@
+package pgp.week4.milk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pgp/week4/milk/MilkApplicationTests.java
@@ -0,0 +1,13 @@
+package pgp.week4.milk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
